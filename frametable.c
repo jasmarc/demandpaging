848cb3c3e9f01676690e758b90cf7635407e0ef5
Jason Marcell <jason@jmarcell.com>
2009-04-09 22:31:20 -0400

    work

--- frametable.c
+++ frametable.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdarg.h>
+#include <stdlib.h>
 #include "frametable.h"
 #include "process.h"
 #include "assert.h"
@@ -29,7 +33,7 @@ frame_entry *simulate_fifo(frame_table *ft, int page, int cpu_time, int process_
     int i;
     
     // if the given page is already in the frame table, go ahead and use it.
-    for(i = 0; i < ft->count; ++i) {
+    for(i = ft->count - 1; i >= 0; i--) {
         frame_entry *entry = ft->framelist[i];
         assert(entry != NULL);
         if ((!entry->empty && (entry->page == page)) && (entry->process_id == process_id)) {
@@ -65,32 +69,34 @@ frame_entry *simulate_fifo(frame_table *ft, int page, int cpu_time, int process_
     }
     
     // otherwise we're going to pick a victim frame to evict
-    assert(ft != NULL);
-    assert(ft->framelist != NULL);
-    assert(ft->count > 0);
-    entry3 = ft->framelist[ft->count - 1];
-    assert(entry3 != NULL);
-    int num2 = entry3->cpu_time;
-    int num3 = entry3->frame_id;
-    int j;
+    assert(ft != NULL);                     // Here we're
+    assert(ft->framelist != NULL);          // just grabbing
+    assert(ft->count > 0);                  // the first
+    entry3 = ft->framelist[ft->count - 1];  // frame entry
+    assert(entry3 != NULL);                 // from the 
+    int least_cpu_time = entry3->cpu_time;  // frame list
+    int frame_to_evict = entry3->frame_id;  // 
+    
+    int j; // Now loop through all the frames looking for the one with the minimum cpu_time
     for (j = ft->count - 2; j >= 0; j--) {
         assert(j >= 0);
-        entry3 = ft->framelist[j];
-        if (entry3->cpu_time < num2) {
-            num2 = entry3->cpu_time;
-            num3 = entry3->frame_id;
+        entry3 = ft->framelist[j]; // grab the next one
+        if (entry3->cpu_time < least_cpu_time) { // we're trying to find the min cpu time
+            least_cpu_time = entry3->cpu_time;
+            frame_to_evict = entry3->frame_id;
         }
     }
-    entry2->frame_id = num3;
-    entry3 = ft->framelist[num3];
-    process3 = ProcessArray[entry3->process_id - 1];
-    process3->TotalResidency += cpu_time - entry3->cpu_time;
-    process3->Evicted++;
-    ft->framelist[num3] = entry2;
-    process2 = ProcessArray[process_id - 1];
-    assert(process2 != NULL);
-    process2->PageFaults++;
-    entry2->PageFault = TRUE;
-    printf("Fault with eviction.\n");
+    entry2->frame_id = frame_to_evict;                       // let our new frame entry move in
+    entry3 = ft->framelist[frame_to_evict];                  // this is the frame entry that's moving out
+    process3 = ProcessArray[entry3->process_id - 1];         // this is the process that's moving out
+    process3->TotalResidency += cpu_time - entry3->cpu_time; // update his residency time
+    process3->Evicted++;                                     // increment his evection status
+    ft->framelist[frame_to_evict] = entry2;                  // move our new frame entry in
+    process2 = ProcessArray[process_id - 1];                 // this is the process that's moving in
+    assert(process2 != NULL);                                // this process shouldn't be null
+    process2->PageFaults++;                                  // increment the incoming process' pagefault count
+    entry2->PageFault = TRUE;                                // yes, we had a page fault
+    printf("Fault, evicting page %d of %d from frame %d.\n", \
+        entry3->page, process3->ID, entry3->frame_id);
     return entry2;
 }
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./a4 -i input.txt -m90 -p10 -s40 -j4 -n100 -r fifo
+./a4 -i input.txt -m20 -p10 -s10 -j2 -n10 -r fifo
